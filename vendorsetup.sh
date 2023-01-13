f0ead7bde1b1d0540e762d5f0fffe7b3c512b332
Noctowl709 <beastdark704@gmail.com>
2023-01-13 21:14:39 +0530

    laurel_sprout : Fix padding
    Signed-off-by: Noctowl709 <beastdark704@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -73,8 +73,8 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	# Screen Settings
 	export OF_SCREEN_H=2400
 	export OF_STATUS_H=80
-	export OF_STATUS_INDENT_LEFT=48
-	export OF_STATUS_INDENT_RIGHT=48
+	export OF_STATUS_INDENT_LEFT=40
+	export OF_STATUS_INDENT_RIGHT=40
 	export OF_CLOCK_POS=1
 	export OF_HIDE_NOTCH=1
 	export OF_ALLOW_DISABLE_NAVBAR=0
